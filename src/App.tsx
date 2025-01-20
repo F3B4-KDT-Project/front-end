44850529a01e06ea4dd7e4c12f4bb8ead79a1be7
imi21123 <imi21123@naver.com>
2025-01-21 01:43:49 +0900

    style: prettier 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
-import Board from './pages/Board'
-import Post from './pages/Post'
-import MyPage from './pages/MyPage'
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Board from './pages/Board';
+import Post from './pages/Post';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path='/sign-in' element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
 
-        <Route path='/' element={<Board />} />
-        <Route path='/post' element={<Post />} />
+        <Route path="/" element={<Board />} />
+        <Route path="/post" element={<Post />} />
 
-        <Route path='/my-page' element={<MyPage />} />
+        <Route path="/my-page" element={<MyPage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
