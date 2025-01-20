b25ea93c36e59060aea474a4120681f30ee59a5c
imi21123 <imi21123@naver.com>
2025-01-21 01:09:46 +0900

    feat: auth input 컴포넌트 props type 지정

---
+++ types/Auth.ts
@@ -0,0 +1,9 @@
+export interface AuthInputProps {
+  type: string;
+  id: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+  error: boolean;
+  message: string;
+}
