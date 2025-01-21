2d908770f8c38e221dec80e9b170b50dd7f85a22
imi21123 <imi21123@naver.com>
2025-01-22 03:58:13 +0900

    feat: error, disabled 값 useState로 상태관리

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -5,10 +5,23 @@ import logo_black from '../../assets/icons/logo_black.svg';
 
 const SignIn = () => {
   const [user, setUser] = useState({ id: '', password: '' });
+  const [error, setError] = useState(false);
+  const [disabled, setDisabled] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
     setUser({ ...user, [id]: value });
+  };
+
+  const handleLogin = () => {
+    // 임시 함수: 에러 상태를 true로 설정
+    setError(true);
+
+    // 임시 함수: 로그인 버튼 비활성화
+    setDisabled(true);
+
+    // 추가적인 로직
+    console.log('로그인 시도:', user);
   };
 
   return (
@@ -34,13 +47,13 @@ const SignIn = () => {
           value={user.password}
           onChange={handleChange}
           placeholder="비밀번호를 입력하세요."
-          error={false}
+          error={error}
           message="틀린 비밀번호이거나 없는 계정입니다."
         />
       </form>
 
       <section>
-        <AuthButton onClick={() => {}} disabled={false} text="LOGIN" />
+        <AuthButton onClick={handleLogin} disabled={disabled} text="LOGIN" />
         <div>
           <span>아직 회원이 아니신가요?</span>
           <button>회원가입</button>
