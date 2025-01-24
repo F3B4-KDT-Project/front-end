4f7d3af052094fca747e820ca39bd3fb2de33b3f
jiHeeFlee <esder1310@naver.com>
2025-01-24 22:38:55 +0900

    fix : gitaction build error

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
@@ -10,28 +8,8 @@ const queryClient = new QueryClient();
 
 /* monaco-editor 세팅 */
 
-// 성공
-// (window as any).MonacoEnvironment = {
-//   getWorkerUrl(moduleId: string, label: string) {
-//     switch (label) {
-//       case 'typescript':
-//         return `${window.location.origin}/monaco-editor/vs/language/typescript/ts.worker.js`;
-//       case 'javascript':
-//         return `${window.location.origin}/monaco-editor/vs/language/typescript/ts.worker.js`;
-//       case 'css':
-//         return `${window.location.origin}/monaco-editor/vs/language/css/css.worker.js`;
-//       case 'html':
-//         return `${window.location.origin}/monaco-editor/vs/language/html/html.worker.js`;
-//       case 'json':
-//         return `${window.location.origin}/monaco-editor/vs/language/json/json.worker.js`;
-//       default:
-//         return `${window.location.origin}/monaco-editor/vs/editor/editor.worker.js`;
-//     }
-//   },
-// };
-
 (window as any).MonacoEnvironment = {
-  getWorkerUrl(moduleId: string, label: string) {
+  getWorkerUrl(label: string) {
     switch (label) {
       case 'typescript':
         return '/monaco-editor/vs/language/typescript/ts.worker.js';
@@ -49,11 +27,6 @@ const queryClient = new QueryClient();
 
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  //   <QueryClientProvider client={queryClient}>
-  //     <App />
-  //   </QueryClientProvider>
-  // </StrictMode>
   <QueryClientProvider client={queryClient}>
       <App />
   </QueryClientProvider>
