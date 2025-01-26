3a20a0a6c968bed7a8fe6b81bbe08b9306d53e02
imi21123 <imi21123@naver.com>
2025-01-27 05:22:11 +0900

    style: main 시멘틱 요소 추가

--- src/components/layout/style.ts
+++ src/components/layout/style.ts
@@ -1,10 +1,10 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 export const AppContainer = styled.div`
   display: flex;
   height: 100vh; /* 화면 전체 높이 */
 `;
 
-export const ContentWrapper = styled.div`
+export const ContentWrapper = styled.main`
   flex-grow: 1; /* 나머지 공간을 채움 */
 `;
