29a1def6b86f610669fd3819c0213eb268104805
jihee <esder1310@naver.com>
2025-01-28 20:42:47 +0900

    docs : styled 코드 분리

--- src/components/Post/Editor/IdeEditor.tsx
+++ src/components/Post/Editor/IdeEditor.tsx
@@ -1,17 +1,9 @@
 import React, { useEffect, useState, useRef } from 'react';
-import styled from '@emotion/styled';
-import Editor, { loader
-  //  ,useMonaco
-   } from '@monaco-editor/react';
-
-import { BsFiles,BsDownload,
-  BsFillBrushFill, 
-  // BsLightbulbOffFill
- } from 'react-icons/bs';
-
+import Editor, { loader } from '@monaco-editor/react';
 // Monaco 타입 가져오기
 import * as monaco from 'monaco-editor';
-
+import { BsFillBrushFill } from 'react-icons/bs';
+import { Container,ButtonContainer, CopyButton, SaveButton, HighlightButton, GlobalStyle } from './style';
 
 interface IdeEditorProps {
   defaultLanguage: string;
@@ -226,80 +218,3 @@ const IdeEditor: React.FC<IdeEditorProps> = ({
 };
 
 export default IdeEditor;
-
-
-const Container = styled.div`
-  z-index: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-
-  margin: 0;
-  padding: 0.62rem;
-  box-sizing: border-box;
-
-  width: 47.9375rem;
-  height: 46.125rem;
-
-  p {
-    font-size: 30px;
-  }
-
-  flex: 2;
-
-  border-radius: 0.9375rem;
-  background: var(--background, #2b2b2b);
-  box-shadow: 0px 0px 4px 0px var(--black, #161616) inset;
-`;
-
-const ButtonContainer=styled.div`
-  z-index: 5;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  color: var(--gray);
-`;
-
-const CopyButton=styled(BsFiles)`
-  width: 1.5rem;
-  height: 1.5rem;
-
-  position: absolute;
-  right: 1.5rem;
-  top: 1.5rem;
-`;
-
-const SaveButton=styled(BsDownload)`
-  width: 1.5rem;
-  height: 1.5rem;
-
-  position: absolute;
-  right: 1.5rem;
-  bottom : 1.5rem;
-`;
-
-const HighlightButton = styled.div`
-  position: absolute;
-  z-index: 10;
-  background-color: var(--input);
-  border: none;
-  border-radius: 5px;
-  padding: 5px;
-  cursor: pointer;
-`;
-
-// 전역 스타일 추가 (형광펜 스타일)
-const GlobalStyle = styled.div`
-  .highlighted-code {
-    background-color: rgba(255, 255, 0, 0.5);
-    /* background-color: var(--input); */
-    cursor: pointer;
-  }
-  
-  > svg{
-    color: var(--white);
-  }
-`;
