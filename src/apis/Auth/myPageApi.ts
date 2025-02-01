89c8bdfc44f323fd86f7e90ddc269f8edc70b184
imi21123 <imi21123@naver.com>
2025-02-02 03:24:00 +0900

    refactor: 프로필 정보 수정 api 함수 분리

--- src/apis/Auth/myPageApi.ts
+++ src/apis/Auth/myPageApi.ts
@@ -1,7 +1,39 @@
 import { UserProfileResponse } from '../../models/Auth';
 import { http } from '../httpClient';
 
-// 유저 프로필 정보 가져오기 API 호출 함수
+// 유저 프로필 정보 조회
 export const fetchUserProfile = async (): Promise<UserProfileResponse> => {
   return await http.get<UserProfileResponse>(`/api/auth/profile`);
+};
+
+// 프로필 이미지 수정
+export const updateProfileImage = async (
+  image: File
+): Promise<UserProfileResponse> => {
+  const formData = new FormData();
+  formData.append('image', image);
+
+  return await http.patch(`/api/auth/profile/profile-image`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+};
+
+// 닉네임 수정
+export const updateNickName = async (
+  nickName: string
+): Promise<UserProfileResponse> => {
+  return await http.patch(`/api/auth/profile/nickname`, null, {
+    params: { nickName },
+  });
+};
+
+// 아이디 수정
+export const updateLoginId = async (
+  loginId: string
+): Promise<UserProfileResponse> => {
+  return await http.patch('/api/auth/profile/login-id', null, {
+    params: { loginId },
+  });
 };
--- src/hooks/Auth/useUpdateProfile.ts
+++ src/hooks/Auth/useUpdateProfile.ts
@@ -1,30 +1,19 @@
-import { http } from '../../apis/httpClient';
+import {
+  updateLoginId,
+  updateNickName,
+  updateProfileImage,
+} from '../../apis/Auth/myPageApi';
 
 export const useUpdateProfile = () => ({
   patchUserProfileImage: async (image: File) => {
-    const formData = new FormData();
-    formData.append('image', image);
-
-    const data = await http.patch(`/api/auth/profile/profile-image`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data', // 헤더에 multipart/form-data 설정
-      },
-    });
-
-    return data;
+    return updateProfileImage(image);
   },
 
   patchUserNickName: async (nickName: string) => {
-    const data = await http.patch(`/api/auth/profile/nickname`, null, {
-      params: { nickName },
-    });
-    return data;
+    return updateNickName(nickName);
   },
 
   patchUserLoginId: async (loginId: string) => {
-    const data = await http.patch('/api/auth/profile/login-id', null, {
-      params: { loginId },
-    });
-    return data;
+    return updateLoginId(loginId);
   },
 });
