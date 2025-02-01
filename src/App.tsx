c00d2911fd6a103fc2951ccfbaf5372cc3849332
imi21123 <imi21123@naver.com>
2025-02-02 05:40:18 +0900

    feat: 게시판, 게시글 별 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,15 @@ function App() {
             }
           />
           <Route
-            path="/post"
+            path="/:boardId"
+            element={
+              <RequireAuth>
+                <Board />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/:boardId/:postId"
             element={
               <RequireAuth>
                 <Post />
