fac6273780310ecd565b428532f371b4568dc442
imi21123 <imi21123@naver.com>
2025-02-13 18:29:05 +0900

    refactor: post 디자인 시스템 적용

--- src/pages/Post/style.ts
+++ src/pages/Post/style.ts
@@ -1,4 +1,3 @@
-
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
@@ -9,13 +8,9 @@ export const Container = styled.div`
 
   padding: 2.38rem 1.94rem;
   gap: 2.19rem;
-  box-sizing: border-box; /* 패딩을 크기 계산에 포함 */
 
   width: 100%;
   height: 100%;
-
-  color: var(--white);
-  background-color: var(--background);
 `;
 
 export const Body = styled.div`
@@ -26,13 +21,11 @@ export const Body = styled.div`
 
   width: 100%;
   height: auto;
-  box-sizing: border-box; /* 패딩을 크기 계산에 포함 */
 
   padding: 1.31rem 1.56rem;
   gap: 1.25rem;
 
   border-radius: 2.1875rem;
-  background-color: var(--input);
-  background: var(--input, rgba(218, 218, 218, 0.35));
-  box-shadow: 0px 0px 4px 0px var(--black, #161616);
+  background-color: ${({ theme }) => theme.colors.input};
+  box-shadow: 0px 0px 4px 0px ${({ theme }) => theme.colors.black};
 `;
