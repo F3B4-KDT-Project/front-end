ac0cf1a27e5eec1f479872e8e2ab6a403f148c23
imi21123 <imi21123@naver.com>
2025-02-20 01:50:47 +0900

    style: 로그인 화면 퍼블리싱

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from '../../assets/icons/logo_black.svg';
 import kakaoLogin from '../../assets/icons/kakao_login_medium_wide.png';
-import { LoginContainer } from './style';
+import { HeaderText, LoginContainer, LoginHeader, LoginSection } from './style';
 
 const Login: React.FC = () => {
   return (
     <LoginContainer>
-      <header>
+      <LoginHeader>
         <img src={logo} alt="coedu logo" />
-        <div>
+        <HeaderText>
           <p>
             실시간으로 소통하는 <br />
             참여형 교육 WEB IDE 플랫폼
           </p>
           <h1>Coedu, 코에듀</h1>
-        </div>
-      </header>
-      <section>
+        </HeaderText>
+      </LoginHeader>
+      <LoginSection>
         <button>
           <img src={kakaoLogin} alt="kakao login" />
         </button>
-      </section>
+        <p>카카오 로그인으로 시작해보세요!</p>
+      </LoginSection>
     </LoginContainer>
   );
 };
