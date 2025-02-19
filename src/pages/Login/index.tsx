424e229c9f4d0062bf934aff12ce6fa0da712fe0
imi21123 <imi21123@naver.com>
2025-02-20 01:25:39 +0900

    feat: 로그인 화면 레이아웃 구성

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import logo from '../../assets/icons/logo_black.svg';
+import kakaoLogin from '../../assets/icons/kakao_login_medium_wide.png';
+import { LoginContainer } from './style';
 
-const Login = () => {
+const Login: React.FC = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <LoginContainer>
+      <header>
+        <img src={logo} alt="coedu logo" />
+        <div>
+          <p>
+            실시간으로 소통하는 <br />
+            참여형 교육 WEB IDE 플랫폼
+          </p>
+          <h1>Coedu, 코에듀</h1>
+        </div>
+      </header>
+      <section>
+        <button>
+          <img src={kakaoLogin} alt="kakao login" />
+        </button>
+      </section>
+    </LoginContainer>
   );
 };
 
