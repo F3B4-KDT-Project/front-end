911846cfe7514379fefb79c73d33cac616c4fde2
imi21123 <imi21123@naver.com>
2025-02-22 07:13:20 +0900

    refactor: ButtonSection 컴포넌트로 분리

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import kakaoLogin from '../../assets/icons/kakao_login_medium_wide.png';
-import { LoginContainer, LoginSection } from './style';
+import { LoginContainer } from './style';
 import { Header } from '../../components/login/Header';
+import { ButtonSection } from '../../components/login/ButtonSection';
 
 const Login: React.FC = () => {
   return (
     <LoginContainer>
       <Header />
-      <LoginSection>
-        <button>
-          <img src={kakaoLogin} alt="kakao login" />
-        </button>
-        <p>카카오 로그인으로 시작해보세요!</p>
-      </LoginSection>
+      <ButtonSection />
     </LoginContainer>
   );
 };
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -8,15 +8,3 @@ export const LoginContainer = styled.div`
   height: 100vh;
   gap: 7.37rem;
 `;
-
-export const LoginSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 2.06rem;
-
-  p {
-    ${({ theme }) => theme.fonts.body4};
-    line-height: 1.125rem;
-  }
-`;
