be77cd8302ae835ae89666b3ff19486208dbeb38
imi21123 <imi21123@naver.com>
2025-02-22 07:12:23 +0900

    refactor: LoginHeader 컴포넌트로 분리

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import logo from '../../assets/icons/logo_black.svg';
 import kakaoLogin from '../../assets/icons/kakao_login_medium_wide.png';
-import { HeaderText, LoginContainer, LoginHeader, LoginSection } from './style';
+import { LoginContainer, LoginSection } from './style';
+import { Header } from '../../components/login/Header';
 
 const Login: React.FC = () => {
   return (
     <LoginContainer>
-      <LoginHeader>
-        <img src={logo} alt="coedu logo" />
-        <HeaderText>
-          <p>
-            실시간으로 소통하는 <br />
-            참여형 교육 WEB IDE 플랫폼
-          </p>
-          <h1>Coedu, 코에듀</h1>
-        </HeaderText>
-      </LoginHeader>
+      <Header />
       <LoginSection>
         <button>
           <img src={kakaoLogin} alt="kakao login" />
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -9,31 +9,6 @@ export const LoginContainer = styled.div`
   gap: 7.37rem;
 `;
 
-export const LoginHeader = styled.header`
-  display: flex;
-  gap: 2.69rem;
-  align-items: center;
-
-  img {
-    width: 12.5rem;
-    height: 12.5rem;
-  }
-`;
-
-export const HeaderText = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1.75rem;
-
-  p {
-    ${({ theme }) => theme.fonts.body7};
-  }
-
-  h1 {
-    ${({ theme }) => theme.fonts.header3};
-  }
-`;
-
 export const LoginSection = styled.section`
   display: flex;
   flex-direction: column;
