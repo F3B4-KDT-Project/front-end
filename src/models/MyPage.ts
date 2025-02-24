41f061a059ba12f90abca36ae524fb85cb2bd808
imi21123 <imi21123@naver.com>
2025-02-24 15:43:22 +0900

    refactor: InputProps type을 common에서 mypage로 위치 이동

--- src/models/MyPage.ts
+++ src/models/MyPage.ts
@@ -14,6 +14,16 @@ export interface ProfileDetailsProps {
   detail: string;
 }
 
+export interface InputProps {
+  type: string;
+  id: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+  error?: boolean;
+  message?: string;
+}
+
 export interface ThemeSelectionProps {
   isDarkMode: boolean;
   setIsDarkMode: (theme: boolean) => void;
