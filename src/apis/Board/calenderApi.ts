7e1843429bc52ee56c1d29674038d954fdaf9150
정윤석 <mar0722@naver.com>
2025-02-25 15:38:02 +0900

    fix: 시간 일수 1씩 차이나는 오류 해결

--- src/apis/Board/calenderApi.ts
+++ src/apis/Board/calenderApi.ts
@@ -3,7 +3,9 @@ import { http } from '../httpClient';
 
 export const FetchScheduleListApi = async (boardId: number, date: string) => {
   const token = localStorage.getItem('accessToken');
-  const formattedDate = new Date(date).toISOString().split('T')[0];
+  const dateObj = new Date(date);
+  const formattedDate = `${dateObj.getFullYear()}-${String(dateObj.getMonth() + 1).padStart(2, '0')}-${String(dateObj.getDate()).padStart(2, '0')}`;
+  console.log(formattedDate);
 
   if (!token) {
     throw new Error('토큰이 없습니다. 로그인 상태를 확인해주세요.');
