989bf7c00dde631cbf914d853391f25a800b6636
정윤석 <mar0722@naver.com>
2025-02-28 02:36:32 +0900

    style: post ui 반응형으로 변경

--- src/components/post/Chat/style.ts
+++ src/components/post/Chat/style.ts
@@ -1,8 +1,8 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  width: 29.75rem;
-  height: 46.125rem;
+  width: 100%;
+  height: 100%;
 
   border-radius: 0.9375rem;
   background: ${({ theme }) => theme.colors.gray};
